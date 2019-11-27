2dcf7f4716119aa23ff06fb4c49365de749f101e
larryyu <linuxprobe.org@gmail.com>
2019-11-27 15:55:35 +0800

    依赖跟新,redis读取优化

--- src/main/java/org/linuxprobe/luava/shiro/redis/cache/ShiroRedisCache.java
+++ src/main/java/org/linuxprobe/luava/shiro/redis/cache/ShiroRedisCache.java
@@ -1,160 +1,160 @@
 package org.linuxprobe.luava.shiro.redis.cache;
 
+import org.apache.shiro.cache.Cache;
+import org.apache.shiro.cache.CacheException;
+import org.linuxprobe.luava.cache.impl.redis.RedisCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+
 import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.apache.shiro.cache.Cache;
-import org.apache.shiro.cache.CacheException;
-import org.linuxprobe.luava.cache.impl.RedisCache;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
-
 public class ShiroRedisCache implements Cache<Serializable, Serializable> {
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	/**
-	 * key前缀
-	 */
-	private String keyPrefix = "shiro:cache:";
-	/**
-	 * redis操作类
-	 */
-	private RedisCache redisCache;
-	private JdkSerializationRedisSerializer serializer = new JdkSerializationRedisSerializer();
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    /**
+     * key前缀
+     */
+    private String keyPrefix = "shiro:cache:";
+    /**
+     * redis操作类
+     */
+    private RedisCache redisCache;
+    private JdkSerializationRedisSerializer serializer = new JdkSerializationRedisSerializer();
 
-	/**
-	 * @param redisCache redis缓存
-	 * @param name       缓存名称
-	 */
-	public ShiroRedisCache(RedisCache redisCache, String name) {
-		if (redisCache == null) {
-			throw new NullPointerException("redisCache can't be null");
-		}
-		this.redisCache = redisCache;
-		this.keyPrefix = this.keyPrefix + name + ":";
-		if (logger.isTraceEnabled()) {
-			logger.trace("create shiro redis cache, keyPrefix is '{}'", this.keyPrefix);
-		}
-	}
+    /**
+     * @param redisCache redis缓存
+     * @param name       缓存名称
+     */
+    public ShiroRedisCache(RedisCache redisCache, String name) {
+        if (redisCache == null) {
+            throw new NullPointerException("redisCache can't be null");
+        }
+        this.redisCache = redisCache;
+        this.keyPrefix = this.keyPrefix + name + ":";
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace("create shiro redis cache, keyPrefix is '{}'", this.keyPrefix);
+        }
+    }
 
-	/**
-	 * 根据Key获取缓存中的值
-	 */
-	@Override
-	public Serializable get(Serializable key) throws CacheException {
-		key = this.keyAddPrefix(key);
-		try {
-			if (key == null) {
-				return null;
-			} else {
-				return this.redisCache.get(key);
-			}
-		} catch (Throwable t) {
-			throw new CacheException(t);
-		}
-	}
+    /**
+     * 根据Key获取缓存中的值
+     */
+    @Override
+    public Serializable get(Serializable key) throws CacheException {
+        key = this.keyAddPrefix(key);
+        try {
+            if (key == null) {
+                return null;
+            } else {
+                return this.redisCache.get(key);
+            }
+        } catch (Throwable t) {
+            throw new CacheException(t);
+        }
+    }
 
-	/**
-	 * 往缓存中放入key-value，返回缓存中之前的值
-	 */
-	@Override
-	public Serializable put(Serializable key, Serializable value) throws CacheException {
-		key = this.keyAddPrefix(key);
-		try {
-			this.redisCache.set(key, value);
-			return value;
-		} catch (Throwable t) {
-			throw new CacheException(t);
-		}
-	}
+    /**
+     * 往缓存中放入key-value，返回缓存中之前的值
+     */
+    @Override
+    public Serializable put(Serializable key, Serializable value) throws CacheException {
+        key = this.keyAddPrefix(key);
+        try {
+            this.redisCache.set(key, value);
+            return value;
+        } catch (Throwable t) {
+            throw new CacheException(t);
+        }
+    }
 
-	/**
-	 * 移除缓存中key对应的值，返回该值
-	 */
-	@Override
-	public Serializable remove(Serializable key) throws CacheException {
-		key = this.keyAddPrefix(key);
-		try {
-			Serializable value = this.redisCache.get(key);
-			this.redisCache.delete(key);
-			return value;
-		} catch (Throwable t) {
-			throw new CacheException(t);
-		}
-	}
+    /**
+     * 移除缓存中key对应的值，返回该值
+     */
+    @Override
+    public Serializable remove(Serializable key) throws CacheException {
+        key = this.keyAddPrefix(key);
+        try {
+            Serializable value = this.redisCache.get(key);
+            this.redisCache.delete(key);
+            return value;
+        } catch (Throwable t) {
+            throw new CacheException(t);
+        }
+    }
 
-	/**
-	 * 清空整个缓存
-	 */
-	@Override
-	public void clear() throws CacheException {
-		try {
-			Set<Serializable> keys = this.redisCache.keys(this.keyPrefix + "*");
-			this.redisCache.delete(keys);
-		} catch (Throwable t) {
-			throw new CacheException(t);
-		}
-	}
+    /**
+     * 清空整个缓存
+     */
+    @Override
+    public void clear() throws CacheException {
+        try {
+            Set<String> keys = this.redisCache.scan(this.keyPrefix + "*");
+            this.redisCache.delete(keys);
+        } catch (Throwable t) {
+            throw new CacheException(t);
+        }
+    }
 
-	/**
-	 * 返回缓存大小
-	 */
-	@Override
-	public int size() {
-		try {
-			return this.redisCache.keys(this.keyPrefix + "*").size();
-		} catch (Throwable t) {
-			throw new CacheException(t);
-		}
-	}
+    /**
+     * 返回缓存大小
+     */
+    @Override
+    public int size() {
+        try {
+            return this.redisCache.scan(this.keyPrefix + "*").size();
+        } catch (Throwable t) {
+            throw new CacheException(t);
+        }
+    }
 
-	/**
-	 * 获取缓存中所有的key
-	 */
-	@Override
-	public Set<Serializable> keys() {
-		try {
-			Set<Serializable> allKeys = this.redisCache.keys(this.keyPrefix + "*");
-			for (Serializable key : allKeys) {
-				key.toString().substring(this.keyPrefix.length());
-			}
-			return allKeys;
-		} catch (Throwable t) {
-			throw new CacheException(t);
-		}
-	}
+    /**
+     * 获取缓存中所有的key
+     */
+    @Override
+    public Set<Serializable> keys() {
+        try {
+            Set<String> allKeys = this.redisCache.scan(this.keyPrefix + "*");
+            for (Serializable key : allKeys) {
+                key.toString().substring(this.keyPrefix.length());
+            }
+            return (Set<Serializable>) (Set<?>) allKeys;
+        } catch (Throwable t) {
+            throw new CacheException(t);
+        }
+    }
 
-	/**
-	 * 获取缓存中所有的value
-	 */
-	@Override
-	public Collection<Serializable> values() {
-		try {
-			Collection<Serializable> allvalues = new HashSet<>();
-			Set<Serializable> keys = this.keys();
-			for (Serializable key : keys) {
-				allvalues.add(this.get(key));
-			}
-			return allvalues;
-		} catch (Throwable t) {
-			throw new CacheException(t);
-		}
-	}
+    /**
+     * 获取缓存中所有的value
+     */
+    @Override
+    public Collection<Serializable> values() {
+        try {
+            Collection<Serializable> allvalues = new HashSet<>();
+            Set<Serializable> keys = this.keys();
+            for (Serializable key : keys) {
+                allvalues.add(this.get(key));
+            }
+            return allvalues;
+        } catch (Throwable t) {
+            throw new CacheException(t);
+        }
+    }
 
-	private String keyAddPrefix(Serializable key) {
-		String newKey = null;
-		try {
-			if (key instanceof String || key instanceof Number || key instanceof Character || key instanceof Boolean) {
-				newKey = this.keyPrefix + key;
-			} else {
-				newKey = this.keyPrefix + new String(this.serializer.serialize(key), "UTF-8");
-			}
-		} catch (UnsupportedEncodingException e) {
-			throw new IllegalArgumentException(e);
-		}
-		return newKey;
-	}
+    private String keyAddPrefix(Serializable key) {
+        String newKey = null;
+        try {
+            if (key instanceof String || key instanceof Number || key instanceof Character || key instanceof Boolean) {
+                newKey = this.keyPrefix + key;
+            } else {
+                newKey = this.keyPrefix + new String(this.serializer.serialize(key), "UTF-8");
+            }
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalArgumentException(e);
+        }
+        return newKey;
+    }
 }
--- src/main/java/org/linuxprobe/luava/shiro/redis/session/ShiroRedisSessionDAO.java
+++ src/main/java/org/linuxprobe/luava/shiro/redis/session/ShiroRedisSessionDAO.java
@@ -1,98 +1,94 @@
 package org.linuxprobe.luava.shiro.redis.session;
 
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.UnknownSessionException;
+import org.apache.shiro.session.mgt.eis.AbstractSessionDAO;
+import org.linuxprobe.luava.cache.impl.redis.RedisCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.apache.shiro.session.Session;
-import org.apache.shiro.session.UnknownSessionException;
-import org.apache.shiro.session.mgt.eis.AbstractSessionDAO;
-import org.linuxprobe.luava.cache.impl.RedisCache;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class ShiroRedisSessionDAO extends AbstractSessionDAO {
-	private static Logger logger = LoggerFactory.getLogger(ShiroRedisSessionDAO.class);
+    private static Logger logger = LoggerFactory.getLogger(ShiroRedisSessionDAO.class);
 
-	private static final String sessionKeyPrefix = "shiro:session:";
+    private static final String sessionKeyPrefix = "shiro:session:";
 
-	private RedisCache redisCache;
+    private RedisCache redisCache;
 
-	public ShiroRedisSessionDAO(RedisCache redisCache) {
-		this.redisCache = redisCache;
-	}
+    public ShiroRedisSessionDAO(RedisCache redisCache) {
+        this.redisCache = redisCache;
+    }
 
-	@Override
-	public void update(Session session) throws UnknownSessionException {
-		this.saveSession(session);
-	}
+    @Override
+    public void update(Session session) throws UnknownSessionException {
+        this.saveSession(session);
+    }
 
-	/**
-	 * save session
-	 * 
-	 * @param session
-	 * @throws UnknownSessionException
-	 */
-	private void saveSession(Session session) throws UnknownSessionException {
-		if (session == null || session.getId() == null) {
-			throw new UnknownSessionException("session or session id is null");
-		}
-		long timeOut = session.getTimeout();
-		if (timeOut < 0) {
-			this.redisCache.set(this.getKey(session.getId()), (Serializable) session);
-		} else {
-			this.redisCache.set(this.getKey(session.getId()), (Serializable) session, timeOut / 1000);
-		}
-	}
+    /**
+     * save session
+     *
+     * @param session session
+     */
+    private void saveSession(Session session) throws UnknownSessionException {
+        if (session == null || session.getId() == null) {
+            throw new UnknownSessionException("session or session id is null");
+        }
+        long timeOut = session.getTimeout();
+        if (timeOut < 0) {
+            this.redisCache.set(this.getKey(session.getId()), (Serializable) session);
+        } else {
+            this.redisCache.set(this.getKey(session.getId()), (Serializable) session, timeOut / 1000);
+        }
+    }
 
-	@Override
-	public void delete(Session session) {
-		if (session == null || session.getId() == null) {
-			logger.error("session or session id is null");
-			return;
-		}
-		redisCache.delete(this.getKey(session.getId()));
-	}
+    @Override
+    public void delete(Session session) {
+        if (session == null || session.getId() == null) {
+            ShiroRedisSessionDAO.logger.error("session or session id is null");
+            return;
+        }
+        this.redisCache.delete(this.getKey(session.getId()));
+    }
 
-	@Override
-	public Collection<Session> getActiveSessions() {
-		Set<Session> sessions = new HashSet<Session>();
-		Set<Serializable> keys = this.redisCache.keys(sessionKeyPrefix + "*");
-		if (keys != null && keys.size() > 0) {
-			for (Serializable key : keys) {
-				Session s = (Session) redisCache.get(key);
-				sessions.add(s);
-			}
-		}
-		return sessions;
-	}
+    @Override
+    public Collection<Session> getActiveSessions() {
+        Set<Session> sessions = new HashSet<Session>();
+        Set<String> keys = this.redisCache.scan(ShiroRedisSessionDAO.sessionKeyPrefix + "*");
+        if (keys != null && keys.size() > 0) {
+            for (Serializable key : keys) {
+                Session s = (Session) this.redisCache.get(key);
+                sessions.add(s);
+            }
+        }
+        return sessions;
+    }
 
-	@Override
-	protected Serializable doCreate(Session session) {
-		Serializable sessionId = this.generateSessionId(session);
-		this.assignSessionId(session, sessionId);
-		this.saveSession(session);
-		return sessionId;
-	}
+    @Override
+    protected Serializable doCreate(Session session) {
+        Serializable sessionId = this.generateSessionId(session);
+        this.assignSessionId(session, sessionId);
+        this.saveSession(session);
+        return sessionId;
+    }
 
-	@Override
-	protected Session doReadSession(Serializable sessionId) {
-		if (sessionId == null) {
-			return null;
-		}
-		Session session = redisCache.get(getKey(sessionId));
-		return session;
-	}
+    @Override
+    protected Session doReadSession(Serializable sessionId) {
+        if (sessionId == null) {
+            return null;
+        }
+        return this.redisCache.get(this.getKey(sessionId));
+    }
 
-	/**
-	 * 把key加上前缀
-	 * 
-	 * @param key
-	 * @return
-	 */
-	private String getKey(Serializable sessionId) {
-		String preKey = sessionKeyPrefix + sessionId;
-		return preKey;
-	}
+    /**
+     * 把key加上前缀
+     *
+     * @param sessionId sessionId
+     */
+    private String getKey(Serializable sessionId) {
+        return ShiroRedisSessionDAO.sessionKeyPrefix + sessionId;
+    }
 }
